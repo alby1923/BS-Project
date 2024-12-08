7908f2864ccd08230037ad223bf77874f6cc2ad9
alby1923 <alby01.pecora@gmail.com>
2024-12-08 14:09:06 +0100

    updated

--- 9.FinalModel.R
+++ 9.FinalModel.R
@@ -1,4 +1,6 @@
 library(rstan)
+library(dplyr)
+library(ggplot2)
 
 df_stan <- readRDS('ultimate_dataset.rds')
 
@@ -57,7 +59,7 @@ data_stan <- function(df,variables_to_keep,target_variables,lags){
                     subj = subj,
                     K = length(target_variables),
                     y = y,
-                    y_trasl = y_trasl,
+                    y_trasl = t(y_trasl),
                     X = covariates)
   
   return(data_model)
@@ -93,15 +95,112 @@ chosen_columns <- c(
 
 target_variables <- c("Glucosio","Colesterolo_Hdl","PMAX","Trigliceridi") #PUT THEM IN ORDER OF DEPENDANCY! "Circonferenza_vita"
 
-lags <- c(100,200,300,400) #max window allowed for previous observation in autoregressive component
-data_model <- data_stan(df_reduced,chosen_columns,target_variables,lags)
+df_stan_filtrato <- df_stan %>%
+  group_by(CAI) %>%                  # Raggruppa per paziente
+  filter(n() >= 20) %>%             # Mantieni solo i gruppi con almeno 10 osservazioni
+  ungroup()                         # Rimuovi il raggruppamento
+
+lags <- c(0,180,0,0) #max window allowed for previous observation in autoregressive component, if 0 it means no autoregressive component
+data_model <- data_stan(df_stan_filtrato,chosen_columns,target_variables,lags)
 
 fit = stan(file = 'model_final.stan', 
            data = data_model, 
-           chains = 1, 
-           iter = 20, 
+           chains = 2, 
+           iter = 10, 
            warmup = 5, 
-           cores = 1,
+           cores = 2,
            thin = 1,
            seed = 19)
+#TRACEPLOTS --------------
+dir.create('STAN_Final')
+folder_name <- 'STAN_Final'
+K <- 4 #number of target variables
+P <- length(chosen_columns)
 
+for (k in 1:K) {
+  # Traceplot di beta e sigma_e per ogni target
+  beta_param <- grep(paste0("^beta\\[.*,", k, "\\]"), names(fit@sim$samples[[1]]), value = TRUE)
+  traceplot_beta_sigma_e <- traceplot(fit, pars = c(beta_param, paste0('sigma_e[', k, ']')))
+  ggsave(paste0(folder_name, "/Traceplot_beta_sigma_e_target_", target_variables[k], ".png"), plot = traceplot_beta_sigma_e)
+  
+  # Traceplot per phi (intercetta target-specifica)
+  traceplot_phi <- traceplot(fit, pars = paste0("phi[", k, "]"))
+  ggsave(paste0(folder_name, "/Traceplot_phi_target_", target_variables[k], ".png"), plot = traceplot_phi)
+  
+  # Traceplot per autoregressive coefficient per target (solo per target specifico)
+  traceplot_autoreg_coef <- traceplot(fit, pars = paste0("autoreg_coef[", k, "]"))
+  ggsave(paste0(folder_name, "/Traceplot_autoreg_coef_target_", target_variables[k], ".png"), plot = traceplot_autoreg_coef)
+  
+  # Traceplot di gamma (elementi fuori-diagonale) per ogni target
+  params_gamma <- c()
+  if(k>1){
+    for (i in 1:(k - 1)) {
+      params_gamma <- c(params_gamma, paste0("gamma[", i, ",", k, "]"))
+    }
+    traceplot_gamma <- traceplot(fit, pars = params_gamma)
+    ggsave(paste0(folder_name, "/Traceplot_gamma_target_", target_variables[k], ".png"), plot = traceplot_gamma)
+  }
+}
+
+# Traceplot di alpha per il primo e il secondo paziente
+traceplot_alpha <- traceplot(fit, pars = c("alpha[1]","alpha[2]","alpha[3]","alpha[4]"))
+ggsave(paste0(folder_name, "/Traceplot_alpha_patients.png"), plot = traceplot_alpha)
+
+#RESULTS ANALYSIS -------------------
+bayes_R2 <- function(posterior_samples, target_index) {
+  y_pred <- posterior_samples$y_hat[, , target_index]
+  var_fit <- apply(y_pred, 1, var)
+  var_res <- posterior_samples$sigma_e[, target_index]
+  var_fit / (var_fit + var_res)
+}
+
+posterior_samples <- extract(fit)
+
+for (k in 1:K) {
+  target_name <- target_variables[k]
+  r2 <- bayes_R2(posterior_samples, target_index = k)
+  r2_plot <- ggplot(data.frame(r2 = r2), aes(x = r2)) +
+    geom_histogram(binwidth = 0.0001, fill = "blue", color = "black", alpha = 0.7) +
+    labs(
+      title = paste("Histogram of R2 for ", target_name),
+      x = paste("R2 (", target_name, ")"),
+      y = "Density"
+    ) +
+    theme_minimal()
+  ggsave(paste0(folder_name, "/Bayesian R2 of ", target_name, ".png"), plot = r2_plot, width = 6, height = 4)
+  
+  y_hat <- posterior_samples$y_hat[, , k]
+  y_hat_point <- colMeans(y_hat, na.rm = TRUE)
+  res <- t(df_stan[, target_name] - y_hat_point)
+  rss <- sum(res^2)
+  cat("\nRSS for", target_name, ": ", rss)
+  rmse <- sqrt(rss / nrow(df_stan))
+  cat("\nRMSE for", target_name, ": ", rmse)
+  RMSE_to_sd_ratio <- rmse / sd(t(df_stan[, target_name]))
+  cat("\nRMSE/SD for", target_name, ": ", RMSE_to_sd_ratio)
+  
+  sigma_plot <- stan_dens(fit, pars = paste0("sigma_e[", k, "]")) +
+    labs(
+      title = paste("Posterior Distribution of Sigma for ", target_name),
+      x = paste("Sigma (", target_name, ")"),
+      y = "Density"
+    )
+  ggsave(paste0(folder_name, "/Sigma posterior distribution of ", target_name, ".png"), plot = sigma_plot, width = 6, height = 4)
+  
+  gamma_param <- c()
+  if(k>1){
+  for (i in 1:(k-1)) {
+    gamma_param <- c(gamma_param, paste0("gamma[", i, ",", k, "]"))
+  }
+  }
+  
+  summary_stats <- summary(fit, pars = c(paste0("beta[", 1:P, ",", k, "]") ,
+                                         paste0("sigma_e[", k, "]"), 
+                                         paste0("phi[", k, "]"),
+                                         paste0("autoreg_coef[", k, "]"),
+                                         gamma_param))
+  
+  summary_text <- capture.output(print(summary_stats))
+  writeLines(summary_text, con = paste0(folder_name, "/Summary of ", target_name, ".txt"))
+  cat('\n--------------------------\n')
+}
--- model_final.stan
+++ model_final.stan
@@ -9,6 +9,7 @@ data {
   int<lower=1, upper=N> subj[T];    // Donor ID for each observation
   matrix[T, P] X;                    // Predictor matrix
   matrix[T, K] y;                    // Outcome matrix (T observations, K targets)
+  matrix[T, K] y_trasl; //autoregressive component
 }
 
 parameters {
@@ -19,21 +20,31 @@ parameters {
   
   real mub;                 // Hyperparameter: mean of alpha
   real<lower=0> eta;        // Hyperparameter: SD of alpha
-  
+  vector[K] autoreg_coef_raw; // Y_trasl will be a vector of zeros for three targets
   real<lower=0> sigma_e[K]; // Error SDs for each target
 }
 
 transformed parameters {
   matrix[K, K] gamma;       // Lower triangular gamma matrix
+  vector[K] autoreg_coef; // Set to zero coefficients for targets with no autoregressive component
   
   // Set lower triangular values to sampled values, upper triangular to 0
   for (k in 1:K) {
     for (j in 1:K) {
-      if (j < k) {
-        gamma[k, j] = gamma_raw[k, j]; // Retain sampled value for lower triangular
+      if (j > k) {
+        gamma[k, j] = gamma_raw[k, j]; // Retain sampled value for upper triangular
       } else {
-        gamma[k, j] = 0;              // Set upper triangular to 0
+        gamma[k, j] = 0;              // Set lower triangular to 0
       }
+    }
+  }
+  
+  for(k in 1:K){
+    if (k==2){
+      autoreg_coef[k] = autoreg_coef_raw[k];
+    }
+    else {
+      autoreg_coef[k] = 0;
     }
   }
 }
@@ -47,11 +58,11 @@ model {
   phi ~ normal(0, 5);                  // Target-specific intercepts
   to_vector(beta) ~ normal(0, 5);      // Coefficients for covariates
   
-  // Priors for gamma_raw (lower triangular)
+  // Priors for gamma_raw (upper triangular)
   for (k in 1:K) {
     for (j in 1:K) {
-      if (j < k) {
-        gamma_raw[k, j] ~ normal(0, 5); // Prior for lower triangular values
+      if (j > k) {
+        gamma_raw[k, j] ~ normal(0, 5); // Prior for upper triangular values
       }
     }
   }
@@ -61,17 +72,15 @@ model {
     sigma_e[k] ~ inv_gamma(3, 2);       // Error SD priors
   }
   
+  // Prior for autoregressive coefficient
+  for (k in 1:K) {
+   autoreg_coef_raw[k] ~ normal(0, 2); //we know from previous experiments this value is really near zero
+  }
+  
   // Likelihood
   for (k in 1:K) {
     // Initialize the linear predictor
-    vector[T] mu_k = X * beta[, k] + alpha[subj] + phi[k];
-    
-    // Add dependencies on previous Y's only if k > 1
-    if (k > 1) {
-      for (j in 1:(k-1)) {
-        mu_k += gamma[k, j] * y[, j];
-      }
-    }
+    vector[T] mu_k = X * beta[, k] + alpha[subj] + phi[k] + autoreg_coef[k] * to_vector(y_trasl[, k]) + y * gamma[,k];
     
     // Define the likelihood
     y[, k] ~ normal(mu_k, sigma_e[k]);
@@ -83,13 +92,12 @@ generated quantities {
   
   for (k in 1:K) {
     // Initialize the linear predictor
-    y_hat[, k] = X * beta[, k] + alpha[subj] + phi[k];
-    
-    // Add dependencies on previous Y's only if k > 1
-    if (k > 1) {
-      for (j in 1:(k-1)) {
-        y_hat[, k] += gamma[k, j] * y_hat[, j];
-      }
-    }
+    y_hat[, k] = X * beta[, k] + alpha[subj] + phi[k] + autoreg_coef[k] * to_vector(y_trasl[, k]);
   }
+  
+  // Add contributions from dependencies on previous targets
+  for (k in 2:K) {
+    y_hat[:, k] += y_hat * gamma[, k];
+  }
+  
 }
