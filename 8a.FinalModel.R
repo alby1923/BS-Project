8c181ff470d97c0dcc4000fe4b9eb4c45a739537
alby1923 <alby01.pecora@gmail.com>
2024-12-21 13:34:37 +0100

    last update

--- 8a.FinalModel.R
+++ 8a.FinalModel.R
@@ -4,63 +4,97 @@ library(ggplot2)
 
 df_stan <- readRDS('ultimate_dataset.rds')
 
-data_stan <- function(df,variables_to_keep,target_variables,lags){
+#TEST SET --------
+#we will run our model only on patients with at least 5 observations in the training set (hence, at least 6 in total)
+df_stan <- df_stan %>%
+  group_by(CAI) %>%         
+  filter(n() >= 6) %>%    
+  ungroup()   
+
+#take out from each patient its last observation since we will try to predict that one with our model
+df_stan_last <- df_stan %>%
+  group_by(CAI) %>%          
+  slice_tail(n = 1) %>%      
+  ungroup()                 
+
+#save this version of test set for the stan model, the other one is for classifier
+df_stan_last_model <- df_stan_last
+
+#we remove form the data_stan (train set) the observations in the test set
+df_stan <- df_stan %>%
+  anti_join(df_stan_last, by = colnames(df_stan_last_model))
+
+#for the test set we will only need the target variables and the sex factor since different thresholds are applied to male and females
+target_variables <- c("Circonferenza_vita","Colesterolo_Hdl","Trigliceridi","PMAX","Glucosio")
+df_stan_last <- df_stan_last[,c(target_variables,'SESSO')]
+df_stan_last <- exp(df_stan_last) #we get back to original values
+table(df_stan_last$SESSO) #just to see with which value males and females are saved in the dataset
+df_stan_last$SESSO <- ifelse(df_stan_last$SESSO < 1, "M", "F")
+df_stan_last$SESSO <- as.factor(df_stan_last$SESSO)
+
+#here set 1 if value for that target we have value in rsnge for metabolic syndrome, zero otherwise
+df_stan_last <- df_stan_last %>%
+  # Glucosio > 100
+  mutate(glucosio_flag = if_else(Glucosio > 100, 1, 0)) %>%
+  # Trigliceridi > 150
+  mutate(trigliceridi_flag = if_else(Trigliceridi > 150, 1, 0)) %>%
+  # Pressione massima (PMAX) > 130
+  mutate(pmax_flag = if_else(PMAX > 130, 1, 0)) %>%
+  # Circonferenza vita > 102 maschi, > 88 femmine
+  mutate(circonferenza_flag = if_else(
+    (SESSO == 'M' & Circonferenza_vita > 102) | 
+      (SESSO == 'F' & Circonferenza_vita > 88), 
+    1, 
+    0
+  )) %>%
+  # Colesterolo < 40 maschi, < 50 femmine
+  mutate(colesterolo_flag = if_else(
+    (SESSO == 'M' & Colesterolo_Hdl < 40) | 
+      (SESSO == 'F' & Colesterolo_Hdl < 50), 
+    1, 
+    0
+  )) %>%
   
-  df_trans_list <- list()
-  for (i in seq_along(lags)) {
-    df1 <- df %>%
-      group_by(CAI) %>%
-      arrange(delta_date, .by_group = TRUE) %>%
-      mutate(
-        successive_timestamps = if_else(
-          delta_date == 0,
-          0,               
-          delta_date - lag(delta_date, default = 0)
-        ),
-        ar_flag = if_else(
-          successive_timestamps <= lags[i] & successive_timestamps != 0, #if zero it is the first observation of the donor
-          1, 
-          0 #if less than zero it means that in delta_date - lag(delta_date, default = 0) it took the last observation of the previous donor
-        ))%>%
-      ungroup()  
-    df_trans_list[[i]] <- df1
-  }
-  
-  y_trasl <- matrix(0, nrow = length(target_variables), ncol = dim(df)[1])
-  
-  k = 0
-  for (target in target_variables){
-    k = k+1
-    df2 <- df_trans_list[[k]]
-    response <- df[[target]]
-    aux <- df2[['ar_flag']] * lag(response, default = 0)
-    for(j in 1:length(aux)){
-      y_trasl[k,j] <- aux[j]
-    }
-  }
-  
+  # Calcolo del punteggio complessivo di rischio
+  mutate(risk_score = rowSums(across(ends_with("_flag"))))
+
+#set 1 if at least three targets in range, hence metabolic syndrome
+num_high_risk <- df_stan_last %>%
+  filter(risk_score >= 3) %>%
+  nrow()
+
+#wee see how many of them and percentage with respect to total number of patients
+num_high_risk
+num_high_risk/dim(df_stan_last)[1]
+
+#save datasets for following models
+saveRDS(df_stan,'df_train_set.rds')
+saveRDS(df_stan_last_model,'df_test_set.rds')
+saveRDS(df_stan_last,'risk_scores_real.rds')
+#STAN MODEL ---------------
+data_stan <- function(df,variables_to_keep,response,df_test){
   #data needed for the model
+  
+  #total number of patients
   patients_mat <- as.matrix(unique(df[,1]))
   patients <- as.vector(patients_mat)
   N <- dim(patients_mat)[1]
-  T <- dim(df)[1]
-  #data <- as.vector(df[,response]) #save response data
-  covariates <- df[,variables_to_keep] #remove response (and useless) covariates
   
-  #tot_obs_patient <- table(df$CAI)
+  #total number of observations
+  T <- dim(df)[1]
+  
+  #covariates and number of covariates
+  covariates <- df[,variables_to_keep] #remove response (and useless) covariates
+  covariates_test <- df_test[,variables_to_keep]
+  P <- dim(covariates)[2]
+  
+  #index of patient related to each line of covariates (for random intercept)
   tot_obs_patient <- as.vector(table(df$CAI)[match(patients_mat, names(table(df$CAI)))])
   subj <- rep(seq_along(tot_obs_patient), times = tot_obs_patient)
+  subj_test <- 1:dim(df_test)[1]
   
-  y <- df[,target_variables]
-  
-  data_model = list(N = N, 
-                    P = dim(covariates)[2], 
-                    T = T, 
-                    subj = subj,
-                    K = length(target_variables),
-                    y = y,
-                    y_trasl = t(y_trasl),
-                    X = covariates)
+  data_model = list(N = N, P = P, T = T, 
+                    subj = subj, y = response, X = covariates, Xtest = covariates_test, subj_test = subj_test)
   
   return(data_model)
 }
@@ -87,23 +121,19 @@ chosen_columns <- c(
   #"Trigliceridi",
   "Volume_medio",
   #"Circonferenza_vita",
-  "Rh",
-  "SESSO",
-  "AB0",
+  #"Rh",
+  #"SESSO",
+  #"AB0",
   'eta_std'
 )
 
-target_variables <- c("Glucosio","Colesterolo_Hdl","PMAX","Trigliceridi") #PUT THEM IN ORDER OF DEPENDANCY! "Circonferenza_vita"
+#for waist circumference we have no autoregressive component
+response <- df_stan$Circonferenza_vita
+response_test <- df_stan_last_model$Circonferenza_vita
+data_model <- data_stan(df_stan,chosen_columns,response,df_stan_last_model)
 
-df_stan_filtrato <- df_stan %>%
-  group_by(CAI) %>%                  # Raggruppa per paziente
-  filter(n() >= 20) %>%             # Mantieni solo i gruppi con almeno 20 osservazioni
-  ungroup()                         # Rimuovi il raggruppamento
-
-lags <- c(0,180,0,0) #max window allowed for previous observation in autoregressive component, if 0 it means no autoregressive component
-data_model <- data_stan(df_stan_filtrato,chosen_columns,target_variables,lags)
-
-fit = stan(file = 'model_final.stan', 
+#fit the model
+fit = stan(file = 'model_final_A.stan', 
            data = data_model, 
            chains = 2, 
            iter = 1000, 
@@ -112,203 +142,118 @@ fit = stan(file = 'model_final.stan',
            thin = 1,
            seed = 19)
 
-#TRACEPLOTS --------------
-dir.create('STAN_Final')
-folder_name <- 'STAN_Final'
-K <- 4 #number of target variables
-P <- length(chosen_columns)
+#TRACEPLOTS AND RESULTS --------------
+#create folder to save results
+dir.create('STAN_Final_Circonferenza_vita')
+folder_name <- 'STAN_Final_Circonferenza_vita'
+file_path <- file.path(folder_name, "final_model_Circonferenza_vita.Rdata") 
+save(fit,file = file_path)
 
-for (k in 1:K) {
-  # Traceplot di beta e sigma_e per ogni target
-  beta_param <- grep(paste0("^beta\\[.*,", k, "\\]"), names(fit@sim$samples[[1]]), value = TRUE)
-  traceplot_beta_sigma_e <- traceplot(fit, pars = c(beta_param, paste0('sigma_e[', k, ']')))
-  ggsave(paste0(folder_name, "/Traceplot_beta_sigma_e_target_", target_variables[k], ".png"), plot = traceplot_beta_sigma_e)
-  
-  # Traceplot per phi (intercetta target-specifica)
-  traceplot_phi <- traceplot(fit, pars = paste0("phi[", k, "]"))
-  ggsave(paste0(folder_name, "/Traceplot_phi_target_", target_variables[k], ".png"), plot = traceplot_phi)
-  
-  # Traceplot per autoregressive coefficient per target (solo per target specifico)
-  traceplot_autoreg_coef <- traceplot(fit, pars = paste0("autoreg_coef[", k, "]"))
-  ggsave(paste0(folder_name, "/Traceplot_autoreg_coef_target_", target_variables[k], ".png"), plot = traceplot_autoreg_coef)
-  
-  # Traceplot di gamma (elementi fuori-diagonale) per ogni target
-  params_gamma <- c()
-  if(k>1){
-    for (i in 1:(k - 1)) {
-      params_gamma <- c(params_gamma, paste0("gamma[", i, ",", k, "]"))
-    }
-    traceplot_gamma <- traceplot(fit, pars = params_gamma)
-    ggsave(paste0(folder_name, "/Traceplot_gamma_target_", target_variables[k], ".png"), plot = traceplot_gamma)
-  }
-}
+#save traceplot of beta and sigma
+q <- traceplot(fit, pars = c(paste0("beta[", 1:length(chosen_columns), "]"), "sigma_e"))
+ggsave(paste0(folder_name, "/Traceplots of Circonferenza_vita.png"), plot = q, width = 6, height = 4)
 
-# Traceplot di alpha per il primo e il secondo paziente
-traceplot_alpha <- traceplot(fit, pars = c("alpha[1]","alpha[2]","alpha[3]","alpha[4]"))
-ggsave(paste0(folder_name, "/Traceplot_alpha_patients.png"), plot = traceplot_alpha)
+#save plot posterior distribution of sigma
+p <- stan_dens(fit, pars = "sigma_e")
+ggsave(paste0(folder_name, "/Sigma posterior distribution of Circonferenza_vita.png"), plot = p, width = 6, height = 4)
 
-#RESULTS ANALYSIS -------------------
-bayes_R2 <- function(posterior_samples, target_index) {
-  y_pred <- posterior_samples$y_hat[, , target_index]
+#extract results
+output <- extract(fit)
+y_train <- output$y_train
+y_test <- output$y_test
+sigma <- output$sigma_e
+
+bayes_R2 <- function(y_pred,sigma_e) {
   var_fit <- apply(y_pred, 1, var)
-  var_res <- posterior_samples$sigma_e[, target_index]
+  var_res <- sigma_e
   var_fit / (var_fit + var_res)
 }
 
-posterior_samples <- extract(fit)
-
-for (k in 1:K) {
-  target_name <- target_variables[k]
-  r2 <- bayes_R2(posterior_samples, target_index = k)
-  r2_plot <- ggplot(data.frame(r2 = r2), aes(x = r2)) +
-    geom_histogram(binwidth = 0.0001, fill = "blue", color = "black", alpha = 0.7) +
-    labs(
-      title = paste("Histogram of R2 for ", target_name),
-      x = paste("R2 (", target_name, ")"),
-      y = "Density"
-    ) +
-    theme_minimal()
-  ggsave(paste0(folder_name, "/Bayesian R2 of ", target_name, ".png"), plot = r2_plot, width = 6, height = 4)
-  
-  y_hat <- posterior_samples$y_hat[, , k]
-  y_hat_point <- colMeans(y_hat, na.rm = TRUE)
-  res <- t(df_stan[, target_name] - y_hat_point)
-  rss <- sum(res^2)
-  cat("\nRSS for", target_name, ": ", rss)
-  rmse <- sqrt(rss / nrow(df_stan))
-  cat("\nRMSE for", target_name, ": ", rmse)
-  RMSE_to_sd_ratio <- rmse / sd(t(df_stan[, target_name]))
-  cat("\nRMSE/SD for", target_name, ": ", RMSE_to_sd_ratio)
-  
-  sigma_plot <- stan_dens(fit, pars = paste0("sigma_e[", k, "]")) +
-    labs(
-      title = paste("Posterior Distribution of Sigma for ", target_name),
-      x = paste("Sigma (", target_name, ")"),
-      y = "Density"
-    )
-  ggsave(paste0(folder_name, "/Sigma posterior distribution of ", target_name, ".png"), plot = sigma_plot, width = 6, height = 4)
-  
-  gamma_param <- c()
-  if(k>1){
-  for (i in 1:(k-1)) {
-    gamma_param <- c(gamma_param, paste0("gamma[", i, ",", k, "]"))
-  }
-  }
-  
-  summary_stats <- summary(fit, pars = c(paste0("beta[", 1:P, ",", k, "]") ,
-                                         paste0("sigma_e[", k, "]"), 
-                                         paste0("phi[", k, "]"),
-                                         paste0("autoreg_coef[", k, "]"),
-                                         gamma_param))
-  
-  summary_text <- capture.output(print(summary_stats))
-  writeLines(summary_text, con = paste0(folder_name, "/Summary of ", target_name, ".txt"))
-  cat('\n--------------------------\n')
-}
-
-#RISK FACTORS -----------------
-
-target_variables <- c("Colesterolo_Hdl","Circonferenza_vita","Glucosio","PMAX","Trigliceridi")
-df_stan_clusters <- df_stan
-
-for (target in target_variables) {
-  df_stan_clusters[[target]] <- exp(df_stan_clusters[[target]])
-}
-
-# Definizione delle soglie per ogni variabile
-glucosio_threshold <- 100
-trigliceridi_threshold <- 150
-pmax_threshold <- 130
-circonferenza_vita_maschi_threshold <- 102
-circonferenza_vita_femmine_threshold <- 88
-colesterolo_maschi_threshold <- 40
-colesterolo_femmine_threshold <- 50
-
-# Creazione della variabile risk_factor
-df_stan_clusters$risk_factor <- NA  # inizializza la variabile
-
-# Funzione per calcolare il rischio
-for (i in 1:nrow(df_stan_clusters)) {
-  # Estrai i valori per ogni osservazione
-  glucosio <- df_stan_clusters$Glucosio[i]
-  trigliceridi <- df_stan_clusters$Trigliceridi[i]
-  pmax <- df_stan_clusters$PMAX[i]
-  circonferenza_vita <- df_stan_clusters$Circonferenza_vita[i]
-  colesterolo <- df_stan_clusters$Colesterolo_Hdl[i]
-  sesso <- df_stan_clusters$SESSO[i]
-  
-  # Determina il sesso: se negativo, è maschio, altrimenti femmina
-  if (sesso < 0) {
-    sesso <- "maschio"
-    circonferenza_threshold <- circonferenza_vita_maschi_threshold
-    colesterolo_threshold <- colesterolo_maschi_threshold
-  } else {
-    sesso <- "femmina"
-    circonferenza_threshold <- circonferenza_vita_femmine_threshold
-    colesterolo_threshold <- colesterolo_femmine_threshold
-  }
-  
-  # Verifica le condizioni per ciascuna variabile
-  risk_count <- 0
-  
-  # Condizioni per glucosio
-  if (glucosio > glucosio_threshold) risk_count <- risk_count + 1
-  
-  # Condizioni per trigliceridi
-  if (trigliceridi > trigliceridi_threshold) risk_count <- risk_count + 1
-  
-  # Condizioni per pmax
-  if (pmax > pmax_threshold) risk_count <- risk_count + 1
-  
-  # Condizioni per circonferenza_vita
-  if (circonferenza_vita > circonferenza_threshold) risk_count <- risk_count + 1
-  
-  # Condizioni per colesterolo
-  if (colesterolo < colesterolo_threshold) risk_count <- risk_count + 1
-  
-  # Assegna il rischio in base al numero di condizioni soddisfatte
-  if (risk_count >= 3) {
-    df_stan_clusters$risk_factor[i] <- "HIGH"
-  } else if (risk_count == 2) {
-    df_stan_clusters$risk_factor[i] <- "MEDIUM"
-  } else {
-    df_stan_clusters$risk_factor[i] <- "LOW"
-  }
-}
-
-# Carica ggplot2 se non è già caricato
-library(ggplot2)
-
-# Creiamo un grafico a barre che mostra la distribuzione della variabile risk_factor
-ggplot(df_stan_clusters, aes(x = risk_factor, fill = risk_factor)) +
-  geom_bar(stat = "count", show.legend = FALSE) +  # Conta le osservazioni per ogni categoria
-  scale_fill_manual(values = c("HIGH" = "red", "MEDIUM" = "yellow", "LOW" = "green")) +  # Colori personalizzati per ogni categoria
+r2 <- bayes_R2(y_train,sigma)
+k <- ggplot(data.frame(r2 = r2), aes(x = r2)) +
+  geom_histogram(binwidth = 0.0001, fill = "blue", color = "black", alpha = 0.7) +
   labs(
-    title = "Distribuzione del fattore di rischio",  # Titolo del grafico
-    x = "Fattore di rischio",  # Etichetta asse X
-    y = "Numero di osservazioni"  # Etichetta asse Y
+    title = paste("Histogram of Circonferenza_vita"),
+    x = 'Circonferenza_vita',
+    y = "Density"
   ) +
-  theme_minimal() +  # Tema del grafico
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotazione delle etichette sull'asse X
-    plot.title = element_text(hjust = 0.5),  # Centra il titolo
-    axis.title = element_text(size = 12),  # Imposta la dimensione del titolo dell'asse
-    axis.text = element_text(size = 10)  # Imposta la dimensione del testo sugli assi
-  )
+  theme_minimal()
+ggsave(paste0(folder_name, "/Bayesian R2 of Circonferenza_vita.png"), plot = k, width = 6, height = 4)
 
-#CLUSTERS --------
+#analyze performance on train set
+y_hat_point <- colMeans(y_train, na.rm = TRUE)
+res <- t(response - y_hat_point)
 
-data_for_model_clusters <- list(
-  N = nrow(df_stan),
-  D = length(chosen_columns),
-  X = df_stan[,chosen_columns]
+cat('\nTRAIN SET')
+rss <- sum(res^2)
+cat('\nRSS: ',rss)
+rmse <- sqrt(rss/dim(df_stan)[1]) 
+cat('\nRMSE: ',rmse)
+RMSE_to_sd_ratio <- rmse / sd(t(response))
+cat('\nRMSE/SD: ',RMSE_to_sd_ratio)
+
+#analyze performance on test set
+y_test_point <- colMeans(y_test, na.rm = TRUE)
+res_test <- t(response_test - y_test_point)
+
+cat('\nTEST SET')
+rss_test <- sum(res_test^2)
+cat('\nRSS: ',rss_test)
+rmse_test <- sqrt(rss_test/dim(df_stan_last)[1]) 
+cat('\nRMSE: ',rmse_test)
+RMSE_to_sd_ratio_test <- rmse_test / sd(t(response_test))
+cat('\nRMSE/SD: ',RMSE_to_sd_ratio_test)
+
+#save y_train output for following models
+file_path <- file.path(folder_name, "Train_set_output_Circonferenza_vita.Rdata")
+save(y_hat_point, file = file_path)
+
+#save y_test output for classifier
+file_path <- file.path(folder_name, "Test_set_output_Circonferenza_vita.Rdata")
+save(y_test_point, file = file_path)
+
+#save summary of stan model
+summary_stats <- summary(fit, pars = c("beta","sigma_e"))
+summary_text <- capture.output(print(summary_stats))
+writeLines(summary_text, con = paste0(folder_name, "/Summary of Circonferenza_vita.txt"))
+
+#DATASET FOR CLASSIFIER -----------------
+#Initialize dataset
+classifier_df <- data.frame(
+  Circonferenza_vita_predicted = numeric(nrow(df_stan_last)),
+  Circonferenza_vita_percentage = numeric(nrow(df_stan_last)),
+  Colesterolo_Hdl_predicted = numeric(nrow(df_stan_last)),
+  Colesterolo_Hdl_percentage = numeric(nrow(df_stan_last)),
+  Trigliceridi_predicted = numeric(nrow(df_stan_last)),
+  Trigliceridi_percentage = numeric(nrow(df_stan_last)),
+  PMAX_predicted = numeric(nrow(df_stan_last)),
+  PMAX_percentage = numeric(nrow(df_stan_last)),
+  Glucosio_predicted = numeric(nrow(df_stan_last)),
+  Glucosio_percentage = numeric(nrow(df_stan_last)),
+  Metabolic_syndrome_probability = numeric(nrow(df_stan_last)),
+  Risk_score = df_stan_last$risk_score
 )
 
-fit = stan(file = 'model_clusters.stan', 
-           data = data_for_model_clusters, 
-           chains = 2, 
-           iter = 1000, 
-           warmup = 500, 
-           cores = 2,
-           thin = 1,
-           seed = 19)
+#fill values for waist circumference (exp because they are log transformed)
+classifier_df$Circonferenza_vita_predicted <- exp(y_test_point)
+
+#threshold for metabolic syndrome
+threshold_m <- 102
+threshold_f <- 88
+
+#compute probability y_test_point > threshold for any observation empirically, hence count how many observations were higher than threshold
+for (i in 1:ncol(y_test)) {
+  #take values for sample i
+  samples_i <- exp(y_test[, i])
+  
+  if (df_stan_last$SESSO[i] == 'M') {
+    classifier_df$Circonferenza_vita_percentage[i] <- mean(samples_i > threshold_m)
+  } else {
+    classifier_df$Circonferenza_vita_percentage[i] <- mean(samples_i > threshold_f)
+  }
+}
+
+#save dataset
+saveRDS(classifier_df,'df_classifier.rds')
+
+
---
+++ 8b.FinalModel.R
@@ -0,0 +1,280 @@
+library(rstan)
+library(dplyr)
+library(ggplot2)
+
+#load data
+df_train <- readRDS('df_train_set.rds')
+df_test <- readRDS('df_test_set.rds')
+classifier_df <- readRDS('df_classifier.rds')
+df_stan_last <- readRDS('risk_scores_real.rds')
+#STAN MODEL ---------------
+data_stan <- function(df,target,variables_to_keep,response,threshold,df_test,previous_target,previous_predictions_train,previous_predictions_test){
+  
+  #compute translated vector for autoregressive component and set it to zero if previous observation is older than threshold
+  df <- df %>%
+    group_by(CAI) %>%
+    arrange(delta_date, .by_group = TRUE) %>%
+    mutate(
+      successive_timestamps = if_else(
+        delta_date == 0,
+        0,               
+        delta_date - lag(delta_date, default = 0)
+      ),
+      ar_flag = if_else(
+        successive_timestamps <= threshold & successive_timestamps != 0,
+        1, 
+        0
+      ))%>%
+    ungroup()  
+  
+  y_trasl <- lag(response, default = 0) * df$ar_flag
+  
+  #number of patients and total observations
+  patients_mat <- as.matrix(unique(df[,1]))
+  patients <- as.vector(patients_mat)
+  N <- dim(patients_mat)[1]
+  T <- dim(df)[1]
+  
+  #take covariates and number of covariates for train and test set
+  covariates <- df[,variables_to_keep]
+  covariates_test <- df_test[,variables_to_keep]
+  P <- dim(covariates)[2]
+  
+  #indexes for each patient (for random intercept)
+  tot_obs_patient <- as.vector(table(df$CAI)[match(patients_mat, names(table(df$CAI)))])
+  subj <- rep(seq_along(tot_obs_patient), times = tot_obs_patient)
+  subj_test <- 1:dim(df_test)[1]
+
+  y_old <- df[,previous_target]
+  y_std <- scale(y_old)
+  
+  #to rescale previous predictions it needs to be with the same mean and sd used for both train and test set and same magnitude of observed values
+  col_means <- colMeans(y_old)
+  col_sds <- apply(y_old, 2, sd)
+  previous_predictions_train_std <- (previous_predictions_train - col_means)/col_sds
+  previous_predictions_test_std <- (previous_predictions_test - col_means)/col_sds
+  
+  #compute if there is autoregressive component for predicted value
+  df_tot <- bind_rows(df, df_test) %>%
+    arrange(CAI, delta_date)
+  
+  df_tot <- df_tot %>%
+    group_by(CAI) %>%
+    arrange(delta_date, .by_group = TRUE) %>%
+    mutate(
+      successive_timestamps = if_else(
+        delta_date == 0,
+        0,               
+        delta_date - lag(delta_date, default = 0)
+      ),
+      ar_flag = if_else(
+        successive_timestamps <= threshold & successive_timestamps != 0,
+        1, 
+        0
+      ))%>%
+    ungroup()  
+  
+  df_last <- df_tot %>%
+    group_by(CAI) %>%
+    slice_tail(n = 1) %>%
+    ungroup()
+  
+  df_last_value <- df %>%
+    group_by(CAI) %>%
+    slice_tail(n = 1) %>%
+    ungroup()
+  
+  y_trasl_test <- df_last_value[,target] * df_last$ar_flag
+  
+  data_model = list(N = N, 
+                    P = P, 
+                    T = T, 
+                    subj = subj,
+                    y = response,
+                    y_std = y_std,
+                    y_trasl = y_trasl,
+                    lag = threshold,
+                    X = covariates,
+                    K = length(previous_target),
+                    y_pred_train_std = previous_predictions_train_std,
+                    y_pred_test_std = previous_predictions_test_std,
+                    Xtest = covariates_test,
+                    subj_test = subj_test,
+                    y_trasl_test = unlist(y_trasl_test))
+  
+  return(data_model)
+  
+}
+
+#cut responses
+chosen_columns <- c(
+  "Altezza",
+  #"Colesterolo_Hdl",
+  "Distribuzione_di_volume",
+  "Ematocrito_hct",
+  "Emoglobina_conc_media_mchc",
+  "Emoglobina_hb",
+  "Emoglobina_massa_media_mch",
+  "Eosinofili_perc",
+  "Eritrociti_rbc",
+  #"Glucosio",
+  "Leucociti_wbc",
+  "Linfociti_perc",
+  "Monociti_perc",
+  #"PMAX",
+  "Peso",
+  "Piastrine",
+  "Polso",
+  #"Trigliceridi",
+  "Volume_medio",
+  #"Circonferenza_vita",
+  "Rh",
+  "SESSO",
+  "AB0",
+  'eta_std'
+)
+
+#ORDER: CIRCONFERENZA VITA - TRIGLICERIDI - COLESTEROLO - GLUCOSIO - PMAX, besides the first one iteratively substitute the new target where ### occurs
+
+#take response values
+response <- df_train$PMAX ###
+response_test <- df_test$PMAX ###
+#autoregression threshold
+threshold <- 180 ######### trigliceridi: 365, colesterolo: 365, glucosio: 0, pmax: 180
+#order of targets
+previous_target <- c('Circonferenza_vita','Trigliceridi','Colesterolo_Hdl','Glucosio')
+#previous predicted values by preceding models
+load("Train_set_output_Glucosio.Rdata") ###
+y_old_train <- matrix(y_hat_point, ncol = 1)
+rm(y_hat_point)
+#pp_train <- y_old_train #use this just for trigliceridi since it is the first one for model_B
+load('pp_train.Rdata')
+pp_train <- cbind(pp_train,y_old_train)
+load("Test_set_output_Glucosio.Rdata") ###
+y_old_test <- matrix(y_test_point, ncol = 1)
+rm(y_test_point)
+#pp_test <- y_old_test #use this just for trigliceridi since it is the first one for model_B
+load('pp_test.Rdata')
+pp_test <- cbind(pp_test,y_old_test)
+
+save(pp_train,file = 'pp_train.Rdata')
+save(pp_test,file = 'pp_test.Rdata')
+
+target <- 'PMAX' #put here the target you are running
+data_model <- data_stan(df_train,target,chosen_columns,response,threshold,df_test,previous_target,pp_train,pp_test)
+
+#fit the model
+fit = stan(file = 'model_final_B.stan', 
+           data = data_model, 
+           chains = 2, 
+           iter = 1000, 
+           warmup = 500, 
+           cores = 2,
+           thin = 1,
+           seed = 19)
+
+#TRACEPLOTS AND RESULTS --------------
+#create folder to save results
+dir.create('STAN_Final_PMAX') ###
+folder_name <- 'STAN_Final_PMAX' ###
+file_path <- file.path(folder_name, "final_model_PMAX.Rdata")  ###
+save(fit,file = file_path)
+
+#save traceplot of beta and sigma
+q <- traceplot(fit, pars = c(paste0("beta[", 1:length(chosen_columns), "]"), "sigma_e"))
+ggsave(paste0(folder_name, "/Traceplots of PMAX.png"), plot = q, width = 6, height = 4) ###
+
+#save plot posterior distribution of sigma
+p <- stan_dens(fit, pars = "sigma_e")
+ggsave(paste0(folder_name, "/Sigma posterior distribution of PMAX.png"), plot = p, width = 6, height = 4) ###
+
+#save plot for autoregressive component
+t <- traceplot(fit, pars = "phi")
+ggsave(paste0(folder_name, "/Traceplot of autoregressive coefficient of PMAX.png"), plot = t, width = 6, height = 4) ###
+
+#save plot for gamma
+l <- traceplot(fit, pars = paste0("gamma[", 1:length(previous_target), "]"))
+ggsave(paste0(folder_name, "/Traceplots of gamma of PMAX.png"), plot = l, width = 6, height = 4) ###
+
+#extract results
+output <- extract(fit)
+y_train <- output$y_train
+y_test <- output$y_test
+sigma <- output$sigma_e
+
+bayes_R2 <- function(y_pred,sigma_e) {
+  var_fit <- apply(y_pred, 1, var)
+  var_res <- sigma_e
+  var_fit / (var_fit + var_res)
+}
+
+r2 <- bayes_R2(y_train,sigma)
+k <- ggplot(data.frame(r2 = r2), aes(x = r2)) +
+  geom_histogram(binwidth = 0.0001, fill = "blue", color = "black", alpha = 0.7) +
+  labs(
+    title = paste("Histogram of PMAX"), ###
+    x = 'PMAX', ###
+    y = "Density"
+  ) +
+  theme_minimal()
+ggsave(paste0(folder_name, "/Bayesian R2 of PMAX.png"), plot = k, width = 6, height = 4) ###
+
+#analyze performance on train set
+y_hat_point <- colMeans(y_train, na.rm = TRUE)
+res <- t(response - y_hat_point)
+
+cat('\nTRAIN SET')
+rss <- sum(res^2)
+cat('\nRSS: ',rss)
+rmse <- sqrt(rss/dim(df_train)[1]) 
+cat('\nRMSE: ',rmse)
+RMSE_to_sd_ratio <- rmse / sd(t(response))
+cat('\nRMSE/SD: ',RMSE_to_sd_ratio)
+
+#analyze performance on test set
+y_test_point <- colMeans(y_test, na.rm = TRUE)
+res_test <- t(response_test - y_test_point)
+
+cat('\nTEST SET')
+rss_test <- sum(res_test^2)
+cat('\nRSS: ',rss_test)
+rmse_test <- sqrt(rss_test/dim(df_stan_last)[1]) 
+cat('\nRMSE: ',rmse_test)
+RMSE_to_sd_ratio_test <- rmse_test / sd(t(response_test))
+cat('\nRMSE/SD: ',RMSE_to_sd_ratio_test)
+
+#save y_train output for following models
+file_path <- file.path(folder_name, "Train_set_output_PMAX.Rdata") ###
+save(y_hat_point, file = file_path)
+
+#save y_test output for classifier
+file_path <- file.path(folder_name, "Test_set_output_PMAX.Rdata") ###
+save(y_test_point, file = file_path)
+
+#save summary of stan model
+summary_stats <- summary(fit, pars = c("beta","sigma_e",'phi','gamma'))
+summary_text <- capture.output(print(summary_stats))
+writeLines(summary_text, con = paste0(folder_name, "/Summary of PMAX.txt")) ###
+#DATASET FOR CLASSIFIER -----------------
+
+#fill values for waist circumference (exp because they are log transformed)
+classifier_df$PMAX_predicted <- exp(y_test_point) ###
+
+#threshold for metabolic syndrome
+threshold_m <- 130
+threshold_f <- 130
+
+#compute probability y_test_point > threshold for any observation empirically, hence count how many observations were higher than threshold
+for (i in 1:ncol(y_test)) {
+  #take values for sample i
+  samples_i <- exp(y_test[, i])
+  
+  if (df_stan_last$SESSO[i] == 'M') {
+    classifier_df$PMAX_percentage[i] <- mean(samples_i > threshold_m) ###
+  } else {
+    classifier_df$PMAX_percentage[i] <- mean(samples_i > threshold_f) ###
+  }
+}
+
+#save dataset
+saveRDS(classifier_df,'df_classifier.rds')
